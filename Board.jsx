0e7da8304f8971cbee6501ec4a5a79afb9943692
Matthew Hutter <matthewjhutter@gmail.com>
2025-01-31 14:44:21 -0800

    handle click for which square is filled, start of winner clculation

--- Board.jsx
+++ Board.jsx
@@ -1,3 +1,25 @@
 import {useState} from 'react';
+import CalculateWinner from './Calculate_Winner';
 
+const Board = ({xIsNextPlayer, squares, onSelection}) {
 
+    const handleClick = () => {
+        if (CalculateWinner(squares) || squares[i]) {
+            return;
+        };
+
+        const nextSquares = squares.slice();
+
+        xIsNextPlayer ? nextSquares[i] = "X" : nextSquares[i] = "O";
+
+        onSelection(nextSquares);
+    }
+
+    const winner = CalculateWinner(squares);
+    let status;
+    if (winner) {
+        
+    }
+}
+
+export default Board;
